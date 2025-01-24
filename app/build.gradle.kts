6cfee6a8fc18beb62fab716d37494869883cccec
Hafeezullah Khan <imhafeezkniazi@gmail.com>
2025-01-24 20:48:17 +0500

    Version update 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
             "com.data.recovery.restore.deleted.photos.videos.audios.docs.gallery.remove.duplicate.files"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "13.0"
+        versionCode = 14
+        versionName = "14.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
